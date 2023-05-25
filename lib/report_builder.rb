659c2e9925b936c3f26d39ea38d6b9427640983f
awdem <wdemontmollin@gmail.com>
2023-05-25 14:08:13 +0100

    refactored for fun

--- lib/report_builder.rb
+++ lib/report_builder.rb
@@ -1,22 +1,24 @@
 def report_builder(school_results)
-  school_results_array = school_results.split(', ')
-
+  school_results_array = school_results.split(',').map!(&:strip)
+  return_string = ""
   green_sum = 0
+  amber_sum = 0
   red_sum = 0
 
   school_results_array.each do |result|
     case result
-    when 'Green'
-      green_sum += 1
-    when 'Red'
-      red_sum += 1
+    when 'Green' then green_sum += 1
+    when 'Amber' then amber_sum += 1
+    when 'Red' then red_sum += 1
     end
   end
 
-  return_string = ""
-
   if green_sum != 0
     return_string += "Green: #{green_sum}"
+  end
+
+  if amber_sum != 0 
+    return_string += "\nAmber: #{amber_sum}"
   end
 
   if red_sum != 0 
--- spec/report_builder_spec.rb
+++ spec/report_builder_spec.rb
@@ -13,11 +13,24 @@ RSpec.describe "#report_builder" do
     report = report_builder(school_results)
     expect(report).to eq "Green: 2"
   end
+  it "returns 'Green: 2' when given 'Green, Green'" do
+    school_results = "Green,Green"
+    report = report_builder(school_results)
+    expect(report).to eq "Green: 2"
+  end
 
   it "returns 'Green: 2, Red: 1' when given 'Green, Green, Red'" do
     school_results = "Green, Green, Red"
     report = report_builder(school_results)
     expect(report).to eq "Green: 2\nRed: 1"
   end
+
+  it "returns 'Green: 2, Red: 1' when given 'Green, Green, Red'" do
+    school_results = "Green, Green, Red, Amber"
+    report = report_builder(school_results)
+    expect(report).to eq "Green: 2\nAmber: 1\nRed: 1"
+  end
+
+
 
 end
